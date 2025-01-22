8599a242c08568cdcfb04ad56762499c8a60587c
Jokubas Rusakevicius <jokubrus@hotmail.com>
2025-01-22 16:37:50 +0200

    Adjusting metadata

--- Settings/Constants.cs
+++ Settings/Constants.cs
@@ -5,7 +5,7 @@ internal static class Constants
     internal static class Plugin
     {
         internal const string ModName = "ProtectSpawnerFarms";
-        internal const string ModVersion = "1.0.3";
+        internal const string ModVersion = "1.1.0";
         internal const string Author = "JoksterCube";
         internal const string ModGUID = $"{Author}.{ModName}";
         internal const string Description = "Protect your spawners used in farming from accidental damage.";
--- Thunderstore/CHANGELOG.md
+++ Thunderstore/CHANGELOG.md
@@ -1,3 +1,4 @@
-| `Version` | `Update Notes`    |
-|-----------|-------------------|
-| 1.0.0     | - Initial Release |
+| `Version` | `Update Notes`     |
+|-----------|--------------------|
+| 1.0.0     | - Initial Release. |
+| 1.1.0     | - Added a way to select specific spawners for protection. |
